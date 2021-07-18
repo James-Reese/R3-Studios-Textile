4fabd826a8878ea7879f0add15ac9c7431b37c67
James Reese III <85970634+James-Reese@users.noreply.github.com>
2021-07-18 11:22:34 -0500

    Typo Correction
    line: 3 OG: Version: pre-production v2

--- wep_select.py
+++ wep_select.py
@@ -2,7 +2,7 @@ import time
 import os
 print("Player Items Selection Management Module")
 print('Author: OKNERD/R3 STUDIOS')
-print('Version: pre-production v2')
+print('Version: Pre-Production v2')
 print('Date: 2021')
 
 
