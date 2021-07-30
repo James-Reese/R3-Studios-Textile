759ad003d0dd59a6e692fe2590ec13790546d132
James-Reese <James.Reese@oknerd.me>
2021-07-29 21:18:13 -0500

    Adding additional framework to the project implimenting new modules

---
+++ core_item_library.py
@@ -0,0 +1,74 @@
+import time
+import os
+print("Player Items Selection Management Module")
+print('Author: OKNERD/R3 STUDIOS')
+print('Version: Pre-Production v2')
+print('Date: 2021')
+
+weapons = {
+  "0x0001": {
+    "name": "longsword",
+    "dmg":
+    "value":
+  },
+  "0x0002": {
+    "name": "shortsword",
+    "dmg": damge_module
+    "value":
+  },
+  "0x0003": {
+    "name": "spiked gloves",
+    "dmg":
+    "value":
+  },
+  "0x0004": {
+    "name": "mace",
+    "dmg":
+    "value":
+  },
+  "0x0005": {
+    "name": "halberd",
+    "dmg":
+    "value":
+  },
+  "0x0006": {
+    "name": "staff",
+    "dmg":
+    "value":
+  },
+  "0x0007": {
+    "name": "claws",
+    "dmg":
+    "value": 2007
+  },
+  "0x0008": {
+    "name": "club",
+    "dmg":
+    "value": 2011
+  },
+  "0x0009": {
+    "name": "war hammer",
+    "dmg":
+    "value": 2004
+  },
+  "0x0010": {
+    "name": "war axe",
+    "dmg":
+    "value": 2007
+  },
+  "0x0011": {
+    "name": "spear",
+    "dmg":
+    "value": 2011
+  },
+  "0x0012": {
+    "name": "bow",
+    "dmg":
+    "value": 2011
+  },
+  "child3": {
+    "value": "crossbow",
+    "dmg":
+    "year": 2011
+  }
+}
---
+++ core_magic_library.py
@@ -0,0 +1,6 @@
+import time
+import os
+print("Player Items Selection Management Module")
+print('Author: OKNERD/R3 STUDIOS')
+print('Version: Pre-Production v2')
+print('Date: 2021')
